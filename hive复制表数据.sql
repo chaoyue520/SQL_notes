e542b7ea1ca3345502b43e4d27105224a883072c
chaoyue520 <32260272+chaoyue520@users.noreply.github.com>
2017-11-14 16:51:05 +0800

    备份Hive表数据

---
+++ hive复制表数据.sql
@@ -0,0 +1,27 @@
+#复制Hive表数据到一张新表(备份)
+
+##### 方法 1：速度快
+
+drop table if exists backups_table;
+create table if not exists backups_table
+	as 
+select * from table_name where xxx
+
+
+##### 方法 2：速度非常慢
+hive -e "SQL line" > xxx.txt
+
+# overwrite into表示覆盖
+# into 表示追加
+hive -e "load data local inpath '/home/sas/jiwenchao/ods_mg_case_1112/ods_wallet_riskcontrol_dd_subset/update_wallet_data/update_wallet_riskcontrol_dd_2017_11_13.txt' into table jiwenchao.ods_wallet_riskcontrol_dd_subset ; "
+
+
+##### 方法 3：速度比较慢
+
+# 创建一张相同表结构数据
+create table backups_table like old_table;
+
+# 把 old_table 对应
+hadoop fs -cp afs://xxx.db/old_table/*  afs://xxx.db/backups_table/
+
+
